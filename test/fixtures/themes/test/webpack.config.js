2f64acf06f346c0d7d6aedd449f313b270dcded0
MomoCow <taocheng@qnap.com>
2019-01-31 08:47:16 +0000

    :white_check_mark: Add an integration test for common usage.

--- test/fixtures/themes/test/webpack.config.js
+++ test/fixtures/themes/test/webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  mode: 'development',
   entry: 'main.js',
   output: {
-    filename: 'bundle.js'
+    filename: 'theme-bundle.js'
   }
 }
--- test/unit/config.test.js
+++
@@ -1,5 +0,0 @@
-const test = require('ava')
-
-test.todo('contextual configuration')
-test.todo('contextual entry paths')
-test.todo('precedence')
